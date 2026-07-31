bd7551ebdadedae9e05be726577467be58bee894
Jeefik <jeefik55@gmail.com>
2026-07-31 09:29:06 +0600

    Refactor formatTimeAgo

--- script.js
+++ script.js
@@ -84,28 +84,49 @@ function formatDate(createdAt) {
 
 function formatTimeAgo(createdAt) {
   const timeDifference = Date.now() - createdAt;
+
   const minutes = Math.floor(timeDifference / 60000);
-  const hours = Math.floor(timeDifference / 3600000);
+
+  const hours = Math.floor(minutes / 60);
+
+  const lastOneMinutes = minutes % 10;
+  const lastTwoMinutes = minutes % 100;
+
+  const lastOneHours = hours % 10;
+  const lastTwoHours = hours % 100;
+
+  if (hours >= 24) {
+      return formatDate(createdAt);
+    }
+  if (hours >= 1) {
+
+    if (lastTwoHours >= 11 && lastTwoHours <= 14) {
+      return hours + " часов назад";
+    }
+
+    if (lastOneHours === 1) {
+      return hours + " час назад";
+    }
+    if (lastOneHours >= 2 && lastOneHours <= 4) {
+      return hours + " часа назад";
+    }
+    return hours + " часов назад";
+  }
 
   if (minutes < 1) {
     return "Только что";
   }
-  if (minutes === 1) {
-    return minutes + " минуту назад";
-  }
-  if (minutes < 60) {
+  if (lastTwoMinutes >= 11 && lastTwoMinutes <= 14) {
     return minutes + " минут назад";
   }
-  if (hours >= 24) {
-    return formatDate(createdAt);
+  if (lastOneMinutes === 1) {
+    return minutes + " минуту назад";
   }
-  if (hours === 1) {
-    return hours + " час назад";
+  if (lastOneMinutes >= 2 && lastOneMinutes <= 4) {
+    return minutes + " минуты назад";
   }
-  if (hours === 2 || hours === 3 || hours === 4) {
-    return hours + " часа назад";
-  }
-  return hours + " часов назад";
+
+  return minutes + " минут назад";
 }
 
 function render() {
